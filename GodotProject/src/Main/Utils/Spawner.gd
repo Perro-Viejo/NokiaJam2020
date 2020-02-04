6b885f03f337190c7cbeac94aee523199d47adb6
Carenalga <mapedorr@gmail.com>
2020-02-04 13:16:54 -0500

    Spawners define X movement direction for enemies
    Added a second spawner to Level1.

--- GodotProject/src/Main/Utils/Spawner.gd
+++ GodotProject/src/Main/Utils/Spawner.gd
@@ -1,6 +1,9 @@
 extends Position2D
 class_name Spawner
 
+enum Side {LEFT = -2, MIDDLE = 0, RIGHT = 2}
+export(Side) var side = Side.MIDDLE
+
 var spawning: bool = true
 var enemy_types: Array = [
 	load("res://src/Characters/Enemies/Wolf/Wolf.tscn"),
@@ -14,7 +17,8 @@ func _on_Timer_timeout():
 	
 	# Elegir un enemigo al azar y parirlo
 	enemy_types.shuffle()
-	var enemy = enemy_types[0].instance()
+	var enemy = enemy_types[0].instance() as SimpleObstacle
 
 	enemy.position = position
+	enemy.distance.x = side
 	get_parent().add_child(enemy)
--- GodotProject/src/Obstacles/SimpleObstacle.gd
+++ GodotProject/src/Obstacles/SimpleObstacle.gd
@@ -1,12 +1,16 @@
 extends KinematicBody2D
+class_name SimpleObstacle
 
 export(int) var visibility_depth = -1
 export(int) var speed = 1
-export(Vector2) var distance = Vector2.ONE
+export(int) var y_distance = 0
 
 var passed_time: = 0.0
 var done_steps: int = 0
+var distance: Vector2 = Vector2.ZERO
 
+func _ready() -> void:
+	distance.y = y_distance
 
 func _physics_process(delta):
 	passed_time += delta
@@ -20,3 +24,19 @@ func _physics_process(delta):
 			EventsManager.emit_signal("possum_alerted")
 
 		done_steps += 1
+	
+	if $Sprite.frame == 3 and $CollisionShape2D.disabled:
+		$CollisionShape2D.disabled = false
+
+	var bodies = $Detector.get_overlapping_bodies()
+
+	for body in bodies:
+		if not $CollisionShape2D.disabled and \
+			(body.name == "Opossum" || body.name == "Bounds"):
+			match body.name:
+				"Opossum":
+					print("¡Ay vida hijueputa! me pegué en la pata!")
+				"Bounds":
+					print("Adiós adiocín, se despide un obstaculín")
+			get_parent().remove_child(self)
+			self.queue_free()
