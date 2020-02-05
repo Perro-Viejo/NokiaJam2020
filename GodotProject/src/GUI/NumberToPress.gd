775adeb7444486e7d25ff8b88f307cc3cf9b135d
Carenalga <mapedorr@gmail.com>
2020-02-05 01:21:16 -0500

    Los scripts de audio en su lugar respectivo.
    Ora hay que presionar las teclas reales del Nokia 3310 para ganar el
    mini-juego.

--- GodotProject/src/GUI/NumberToPress.gd
+++ GodotProject/src/GUI/NumberToPress.gd
@@ -7,7 +7,7 @@ var is_good = false;
 
 func _ready():
 	EventsManager.connect("possum_pretended", self, "_on_possum_alerted")	
-	number_to_press = str(int(rand_range(0, 10)))
+	number_to_press = str(randi() % 10)
 	set_text(number_to_press)
 	set_visible(false)
 
@@ -16,14 +16,14 @@ func _input(event: InputEvent):
 	if (event is InputEventKey):
 		var character = (event as InputEventKey).as_text()
 
-		if should_press_number and character.is_valid_integer():
-			if character == number_to_press:
+		if should_press_number:
+			if event.get_action_strength(number_to_press) > 0:
 				is_good=true
 				$InfoText.show_good_message()
-				should_press_number=false
+				should_press_number = false
 			else:
 				is_good=false
-				should_press_number=false
+				should_press_number = false
 				$InfoText.show_bad_message()
 				EventsManager.emit_signal("possum_discovered")
 			
