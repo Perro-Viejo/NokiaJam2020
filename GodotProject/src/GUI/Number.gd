393e8e8bfa754f56f73292c3aa12cecf02ad4c86
José Miguel Romero Espitia <jmromeroe@gmail.com>
2020-02-20 12:22:21 -0500

    Adding playable numbers in the right panel with actual states

--- GodotProject/src/GUI/Number.gd
+++ GodotProject/src/GUI/Number.gd
@@ -12,12 +12,9 @@ var current_state = NUMBER_TO_PRESS_STATES.READY_TO_NEXT;
 
 onready var control_node = get_parent().get_parent();
 onready var right_pane_node = get_parent();
-
+onready var smell_bar = control_node.get_node("LeftPanel/CenterContainer/SmellBar")
 func _ready():
-	EventsManager.connect("possum_pretended", self, "_on_possum_alerted")	
-	number_to_press = str(int(rand_range(0, 10)))
-	$NumberToPress.set_text(number_to_press)
-	set_position(right_pane_node.get_node("InitialPosition").get_position())
+	EventsManager.connect("possum_pretended", self, "_on_possum_alerted")
 	set_visible(true)
 
 func _input(event: InputEvent):
@@ -41,22 +38,29 @@ func _on_Timer_timeout():
 		EventsManager.emit_signal("possum_discovered")
 	else:
 		current_state = NUMBER_TO_PRESS_STATES.READY_TO_NEXT
-		number_to_press = str(int(rand_range(0, 10)))
 		$Tween.interpolate_property(self, "rect_position", right_pane_node.get_node("MiddlePosition").get_position(), right_pane_node.get_node("FinalPosition").get_position(), 1, Tween.TRANS_LINEAR, Tween.EASE_OUT)
 		$Tween.start()
 		
-		$NumberToPress.set_text(number_to_press)
 
-func _on_possum_alerted():
-	set_visible(true)
-	
+func launch_number():
 	current_state = NUMBER_TO_PRESS_STATES.READY_TO_NEXT
+	number_to_press = str(int(rand_range(0, 10)))
+	$NumberToPress.set_text(number_to_press)
 	$Tween.interpolate_property(self, "rect_position", right_pane_node.get_node("InitialPosition").get_position(), right_pane_node.get_node("MiddlePosition").get_position(), 1, Tween.TRANS_LINEAR, Tween.EASE_OUT)
 	$Tween.start()
 	
-	$Timer.start()
-
+func _on_possum_alerted():
+	set_visible(true)
+	launch_number()
 
 func _on_Tween_tween_completed(object, key):
 	if get_position() == right_pane_node.get_node("MiddlePosition").get_position():
 		current_state = NUMBER_TO_PRESS_STATES.SHOULD_PRESS
+		$Timer.start()
+	if get_position() == right_pane_node.get_node("FinalPosition").get_position():
+		if !smell_bar.time_count_started:
+			launch_number()
+		elif smell_bar.time_count > 2:
+			print("here2")
+			launch_number()
+	
--- GodotProject/src/GUI/SmellBar.gd
+++ GodotProject/src/GUI/SmellBar.gd
@@ -5,6 +5,8 @@ var current_smell_time: int = 0
 var max_offset: int = 93
 var min_offset: int = 7
 
+var time_count_started = false
+
 func _ready() -> void:
 	EventsManager.connect('enemy_approached', self, '_on_enemy_approached')
 	$Timer.connect('timeout', self, '_on_timeout')
@@ -13,7 +15,7 @@ func _on_enemy_approached(smell_time) -> void:
 	value = max_offset
 	time_count = smell_time
 	current_smell_time = smell_time
-	
+	time_count_started = true
 	$Timer.start(1)
 	
 func _on_timeout():
@@ -22,4 +24,5 @@ func _on_timeout():
 	
 	if value <= min_offset:
 		$Timer.stop()
+		time_count_started = false
 		EventsManager.emit_signal('possum_done')
